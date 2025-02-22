b16836387f49518fbf4ed6b58a5f0f2061bcc5ab
Mohammad Umar <kvstps2005@gmail.com>
2025-02-23 00:00:07 +0530

    Third Commit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-//Event Delegation Concept used which means not applying eventlistener to all chlild, its delegated to its parent which using bubbling concept.
-//Now Code is very good optimized 
 const root = document.getElementById('root');
 root.addEventListener('click',(event)=>{
    
